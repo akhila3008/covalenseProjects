5e9cb17cde017ac6f633af497575ef96cad2a66a
akhila.reddy <akhila.reddy@AkhilaReddy.covalensedigital.net>
2021-11-17 12:27:40 +0530

    Initial commit:Dependencies are added. commit 1: MySQL properties are added in Application Properties.

---
+++ EmployeeRelationshipMappingEx/src/main/java/com/cov/EmployeeRelationshipMappingExApplication.java
@@ -0,0 +1,13 @@
+package com.cov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRelationshipMappingExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRelationshipMappingExApplication.class, args);
+	}
+
+}
